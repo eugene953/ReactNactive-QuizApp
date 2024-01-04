e9ec2d102655c8c7f45571e9a04a370d7ecf739f
nfoua eugene <nfouaeugene953@gmail.com>
2024-01-04 05:44:45 +0100

    first commit

---
+++ data/questions.js
@@ -0,0 +1,160 @@
+export default [
+    {
+        question:"In what continent is Cameroon",
+        options:
+        [
+            {
+            id:"0",
+            options:"A",
+            answer:"South America",
+
+        },
+        {
+            id:"1",
+            options:"B",
+            answer:"Asia",
+
+        },
+        {
+            id:"2",
+            options:"C",
+            answer:"Jamaica",
+
+        },
+        {
+            id:"3",
+            options:"D",
+            answer:"Africa",
+
+        },
+    ],
+    correctAnswerIndex: 3
+    },
+    {
+        question:"Who is the father of computers",
+        options:
+        [
+            {
+            id:"0",
+            options:"A",
+            answer:"James Gosling",
+
+        },
+        {
+            id:"1",
+            options:"B",
+            answer:"Charles Babbage",
+
+        },
+        {
+            id:"2",
+            options:"C",
+            answer:"Dennis Ritchie",
+
+        },
+        {
+            id:"3",
+            options:"D",
+            answer:"Bjarne Stroustrup ",
+
+        },
+    ],
+    correctAnswerIndex: 1
+    },
+
+    {
+        question:"MS-Word is an example of ______",
+        options:
+        [
+            {
+            id:"0",
+            options:"A",
+            answer:"An operating system",
+
+        },
+        {
+            id:"1",
+            options:"B",
+            answer:"A processing device",
+
+        },
+        {
+            id:"2",
+            options:"C",
+            answer:"Application software",
+
+        },
+        {
+            id:"3",
+            options:"D",
+            answer:"An input device",
+
+        },
+    ],
+    correctAnswerIndex: 2
+    },
+
+    {
+        question:"Crtl, Shift and Alt are called _____",
+        options:
+        [
+            {
+            id:"0",
+            options:"A",
+            answer:"modifier",
+
+        },
+        {
+            id:"1",
+            options:"B",
+            answer:"function",
+
+        },
+        {
+            id:"2",
+            options:"C",
+            answer:"alphanumeric",
+
+        },
+        {
+            id:"3",
+            options:"D",
+            answer:"adjustment",
+
+        },
+    ],
+    correctAnswerIndex: 0
+    },
+
+    {
+        question:"A computer can not boot if it does not have  the ____",
+        options:
+        [
+            {
+            id:"0",
+            options:"A",
+            answer:"Compiler",
+
+        },
+        {
+            id:"1",
+            options:"B",
+            answer:"Loader",
+
+        },
+        {
+            id:"2",
+            options:"C",
+            answer:"Operating System",
+
+        },
+        {
+            id:"3",
+            options:"D",
+            answer:"Assemble",
+
+        },
+    ],
+    correctAnswerIndex: 2
+    },
+]
---
+++ screens/HomeScreen.js
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View, Image ,  Pressable} from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+  return (
+    <View  style={{marginTop:25}}>
+      <Image 
+      style= {{height: 360, width: "100v", resizeMode:"contain" }}
+    source={require ("../assets/quizicon.jpg")} 
+    
+    />
+    <View style={{padding:10}}>
+        <Text style={{textAlign:"center" , color:"magenta", fontSize: 20, fontWeight: "600"}}>
+            QUIZ RULES
+        </Text>
+
+        <View style={{padding:10, backgroundColor:"#f88379", borderRadius:16, marginTop:15}}>
+            <View style={{flexDirection:"row", alignItems:"center" , marginVertical:4}}>
+                <Text style={{ color:"white", paddingBottom:8}}>.</Text>
+                <Text style={{marginLeft:4, color:"#722f37", fontSize:15, fontWeight:"500"}}>
+                    For each correct answer you get 5 points. </Text>
+            </View>
+            
+            <View style={{flexDirection:"row", alignItems:"center", marginVertical:4}}>
+                <Text style={{ color:"white",paddingBottom:25}}>.</Text>
+                <Text style={{marginLeft:4, color:"#722f37", fontSize:15, fontWeight:"500"}}>
+                    There is no negative marking for wrong answer. </Text>
+                    </View>
+
+            <View style={{flexDirection:"row", alignItems:"center" , marginVertical:4}}>
+                <Text style={{ color:"white",paddingBottom:8 }}>.</Text>
+                <Text style={{marginLeft:4, color:"#722f37", fontSize:15, fontWeight:"500"}}>
+                    Each question has a time limit of 15 second. </Text>
+                    </View>
+
+            <View style={{flexDirection:"row", alignItems:"center" , marginVertical:4}}>
+                <Text style={{ color:"white", paddingBottom:25}}>.</Text>
+                <Text style={{marginLeft:4, color:"#722f37", fontSize:15, fontWeight:"500"}}>
+                    You should answer all the questions compusoliry. </Text>
+          
+
+          </View>
+        </View>
+    </View>
+      
+         <Pressable 
+         onPress = { () => navigation.navigate("Quiz")}
+         style={{backgroundColor:"magenta",padding: 14, width:"120",borderRadius:25, marginLeft:"auto",marginRight:"auto",marginTop:30,  }}>
+            <Text style={{color:"white", fontWeight:"600", textAlign:"center",}}>Start Quiz</Text>
+
+         </Pressable>
+
+
+
+      
+      
+      </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
